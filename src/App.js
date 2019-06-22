8d93e3bb2267bb24e17dbd7cc308113610b5ca71
Sean McPherson <seanmcp25@gmail.com>
2019-06-21 20:51:24 -0400

    Implement redux-esque hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { useStore } from './store'
+import { setName } from './store/actions'
 
 function App() {
-    return <div>Hook Playground</div>
+    const [state, actions] = useStore({ setName })
+    return (
+        <main>
+            <h1>Hook Playground</h1>
+            <p>Name: {state.name}</p>
+            <button onClick={() => actions.setName('Anne')}>Change name</button>
+        </main>
+    )
 }
 
 export default App
